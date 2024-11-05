edef7ce4ea0fb93769cd42527181740cc6ca5331
김정현 (Kim JungHyeon) <junghyeonkim.dev@gmail.com>
2024-11-05 14:25:22 +0900

    Next 튜토리얼 1~5 장

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,7 +1,10 @@
+import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${inter.className}`}>{children}</body>
     </html>
   );
 }
